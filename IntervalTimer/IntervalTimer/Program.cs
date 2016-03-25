413b652034f6e94d4c96b0edde06bd1f2ca3fa49
Sharon Cichelli <girlwritescode@gmail.com>
2016-03-24 21:28:47 -0500

    Initial commit: console app plays a sound

---
+++ IntervalTimer/IntervalTimer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Media;
+using System.Reflection;
+
+namespace IntervalTimer
+{
+    class Program
+    {
+        static SoundPlayer _player;
+
+        static void Main(string[] args)
+        {
+            InitializePlayer();
+            _player.Play();
+            Console.ReadLine();
+        }
+
+        private static void InitializePlayer()
+        {
+            _player = new SoundPlayer(Assembly.GetExecutingAssembly().GetManifestResourceStream("IntervalTimer.tada.wav"));
+        }
+    }
+}

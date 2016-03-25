57ea1f5a87078f611ff42b5b64a96d743a0b09a9
Sharon Cichelli <girlwritescode@gmail.com>
2016-03-24 23:36:28 -0500

    extract classes to their own files

---
+++ IntervalTimer/IntervalTimer/Notifier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IntervalTimer
+{
+    public interface Notifier
+    {
+        void Info(string message);
+    }
+
+    public class ConsoleNotifier : Notifier
+    {
+        public void Info(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ IntervalTimer/IntervalTimer/Sounder.cs
@@ -0,0 +1,25 @@
+﻿using System.Media;
+using System.Reflection;
+
+namespace IntervalTimer
+{
+    public interface Sounder
+    {
+        void Play();
+    }
+
+    public class SystemSounder : Sounder
+    {
+        private SoundPlayer _player;
+
+        public SystemSounder(string soundFile)
+        {
+            _player = new SoundPlayer(Assembly.GetExecutingAssembly().GetManifestResourceStream(soundFile));
+        }
+
+        public void Play()
+        {
+            _player.Play();
+        }
+    }
+}
